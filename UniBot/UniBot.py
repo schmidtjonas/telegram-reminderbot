8cb79b500244705d92526594a19d2f23322e6598
Jonas Schmidt <jonas.schmidt@student.hpi.uni-potsdam.de>
2018-12-16 19:44:57 +0100

    save

--- UniBot/UniBot.py
+++ UniBot/UniBot.py
@@ -19,26 +19,27 @@ class UniBot:
 
 	def add(self, bot, update, args):
 		if len(args) != 1:
-			errorHandler(update, "Gib bitte genau ein Fach an!")
+			self.errorHandler(update, "Gib bitte genau ein Fach an!")
 			return
 		elif args[0] == " ":
-			errorHandler(update, "Gib bitte genau ein Fach an!")
+			self.errorHandler(update, "Gib bitte genau ein Fach an!")
 			return
 		if args[0] not in self.entries:
 			self.entries.append(args[0])
 		else:
-			errorHandler(update, "Fach existiert bereits!")
+			self.errorHandler(update, "Fach existiert bereits!")
+			return
 
 		self.saveEntry(args[0])
 
-		update.message.reply_text(str(args[0]) + 'Wurde hinzugefügt. ')
+		update.message.reply_text(str(args[0]) + ' wurde hinzugefügt. ')
 
 	def errorHandler(self, update, error):
 		update.message.reply_text("Fehler: " + error)
 
 	def deleteFach(self, fach, update):
 		if fach not in self.entries:
-			errorHandler("Verschrieben? Dieses Fach existiert nicht!")
+			self.errorHandler("Verschrieben? Dieses Fach existiert nicht!")
 		else:
 			self.entries.remove(fach)
 
@@ -50,7 +51,7 @@ class UniBot:
 
 	def saveEntry(self, entry):
 		with open(self.file, "a") as file:
-			file.write("\n" + entry)
+			file.write(entry + "\n")
 
 
 b = UniBot()
